ce6c55509c19988c694e80c80937a905642f13f1
EvgenyGRI <eugene_gri@mail.ru>
2021-12-08 14:33:58 +0300

    correct 'postremove' script in rpm

--- deploy/rpm/.postremove.sh
+++ deploy/rpm/.postremove.sh
@@ -3,7 +3,6 @@ if [ "$1" -ge 1 ]; then
 fi
 if [ "$1" = 0 ]; then
   systemctl daemon-reload
-  [ -e "/opt/<project>/<service>" ] && rm "/opt/<project>/<service>"
   [ -e "/var/run/<service>.sock" ] && rm "/var/run/<service>.sock"
 fi
 exit 0
